d2e9b32bd1233c12724cac543e2e544b35ffb4a1
BetaDotDev <158769884+BetaDotDev@users.noreply.github.com>
2025-12-04 13:27:42 +0100

    Update login.php

--- 04-SQL/agenda/login.php
+++ 04-SQL/agenda/login.php
@@ -1,2 +1,20 @@
 <?php
-    
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Agenda - Login</title>
+
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="wrapper">
+
+    </div>
+</body>
+</html>5
