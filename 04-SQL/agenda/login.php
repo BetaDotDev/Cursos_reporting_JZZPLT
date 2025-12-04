84ea77154368c02632789ddd6edbf3d12de6b9ad
BetaDotDev <158769884+BetaDotDev@users.noreply.github.com>
2025-12-04 13:57:27 +0100

    Update login.php

--- 04-SQL/agenda/login.php
+++ 04-SQL/agenda/login.php
@@ -4,17 +4,32 @@
 
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Agenda - Login</title>
+        <title>Agenda - Login</title>
 
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <div class="wrapper">
-
-    </div>
-</body>
-</html>5
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <aside class="side">
+                <div>
+                    <div class="side-logo">
+                        <div class="side-badge">Ag</div>
+                        <div>Agenda Pro</div>
+                        <h2 class="side-title">Tu agendita de contactos, <br> ordenada y accesible  </h2>
+                        <p class="side-text">
+                            Inicia la sesión para poder gestionar tus churricontactos de forma
+                            sencilla, sin disytacciones y desde cualquier dispositivo.
+                        </p>
+                    </div>
+                    <p class= "side-footer">
+                        BBDD: <strong> agenda</strong> Tablas_ <strong>usuarios</strong>/<strong>contactos</strong>
+                    </p>
+                </div>
+            </aside>
+        </div>
+    </body>
+</html> 
